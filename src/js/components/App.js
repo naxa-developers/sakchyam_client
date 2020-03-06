1cbc1dcf3cc146ce236f450a8790539e6635c791
Varun <deepak.pradhan048@gmail.com>
2020-03-06 11:44:57 +0545

    Functional WEBPACK,ESLINT,PREETIER & PRODUCTION SUCCESSFULL ,CODE CLIMATE APPROVED ,README UPDATED

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <TestComponent data="Sackhyam" />
+        <TestComponent data="Sakchyam" />
         <img alt="sakchyam" src={logo} />
       </div>
     );
