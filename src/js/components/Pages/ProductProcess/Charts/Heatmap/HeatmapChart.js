bf27c65306c4131b792dc7993644cc89e219f130
royallsilwallz <royalsilwal@gmail.com>
2020-07-21 08:07:44 +0545

    Changes in RightSideBar filter and bubble chart filter Product/Process

--- src/js/components/Pages/ProductProcess/Charts/Heatmap/HeatmapChart.js
+++ src/js/components/Pages/ProductProcess/Charts/Heatmap/HeatmapChart.js
@@ -128,8 +128,11 @@ class HeatmapChart extends React.Component {
           // trim: true,
           // hideOverlappingLabels: false,
           align: 'right',
-          minWidth: !this.props.activeModal ? 150 : 400,
-          maxWidth: 500,
+          minWidth: 150,
+          // minWidth: 800,
+          // minWidth: !this.props.activeModal ? 150 : 400,
+          // maxWidth: 500,
+          maxWidth: 300,
           style: {
             colors: [],
             fontSize: '12px',
@@ -152,6 +155,19 @@ class HeatmapChart extends React.Component {
       legend: {
         show: false,
       },
+      tooltip: {
+        y: {
+          formatter: undefined,
+          title: {
+            formatter: seriesName => {
+              const res = seriesName
+                .replace(/^[, ]+|[, ]+$|[, ]+/g, ' ')
+                .trim();
+              return res;
+            },
+          },
+        },
+      },
     };
 
     this.setState({ options });
@@ -162,8 +178,161 @@ class HeatmapChart extends React.Component {
       productProcessReducer: { heatMapData },
     } = this.props;
 
+    const options = {
+      chart: {
+        toolbar: { show: false },
+        height: 350,
+        type: 'heatmap',
+        // width: 900,
+      },
+      plotOptions: {
+        heatmap: {
+          shadeIntensity: 0.5,
+          radius: 0,
+          // useFillColorAsStroke: true,
+          useFillColorAsStroke: false,
+          colorScale: {
+            ranges: [
+              {
+                from: 0,
+                to: 1,
+                name: '',
+                color: '#F9EBEA',
+              },
+              {
+                from: 1,
+                to: 2,
+                name: '',
+                color: '#E6B0AA',
+              },
+              {
+                from: 2,
+                to: 3,
+                name: '',
+                color: '#D98880',
+              },
+              {
+                from: 4,
+                to: 5,
+                name: '',
+                color: '#C0392B',
+              },
+              {
+                from: 6,
+                to: 7,
+                name: '',
+                color: '#A93226',
+                // color: '',
+              },
+              {
+                from: 8,
+                to: 9,
+                name: '',
+                color: '#7B241C',
+              },
+              // {
+              //   from: 0,
+              //   to: 1,
+              //   name: '',
+              //   color: '',
+              // },
+
+              // {
+              //   from: 0,
+              //   to: 1,
+              //   name: 'low',
+              //   color: '#00A100', // green
+              //   // color: '#B1B424', // green
+              // },
+              // {
+              //   from: 2,
+              //   to: 5,
+              //   name: 'medium',
+              //   color: '#128FD9', // blue // #B1B424
+              //   // color: '#007078', // blue // #B1B424
+              // },
+              // {
+              //   from: 6,
+              //   to: 15,
+              //   name: 'high',
+              //   color: '#FFB200',
+              // },
+              // {
+              //   from: 8,
+              //   to: 15,
+              //   name: 'extreme',
+              //   color: '#FF0000',
+              // },
+            ],
+          },
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        width: 1,
+      },
+      // colors: ['#008FFB'],
+      title: {
+        text: undefined,
+      },
+      yaxis: {
+        // labels: {
+        //   // show: false,
+        //   trim: true,
+        //   // hideOverlappingLabels: false,
+        // },
+        labels: {
+          show: true,
+          // trim: true,
+          // hideOverlappingLabels: false,
+          align: 'right',
+          minWidth: 150,
+          // minWidth: 800,
+          // minWidth: !this.props.activeModal ? 150 : 400,
+          // maxWidth: 500,
+          maxWidth: 300,
+          style: {
+            colors: [],
+            fontSize: '12px',
+            fontFamily: 'Helvetica, Arial, sans-serif',
+            fontWeight: 400,
+            cssClass: 'apexcharts-yaxis-label',
+          },
+          offsetX: 0,
+          offsetY: 0,
+          rotate: 0,
+        },
+      },
+      xaxis: {
+        labels: {
+          // show: false,
+          trim: true,
+          hideOverlappingLabels: false,
+        },
+      },
+      legend: {
+        show: false,
+      },
+      tooltip: {
+        y: {
+          formatter: undefined,
+          title: {
+            formatter: seriesName => {
+              const res = seriesName
+                .replace(/^[, ]+|[, ]+$|[, ]+/g, ' ')
+                .trim();
+              return res;
+            },
+          },
+        },
+      },
+    };
+
     this.setState(preState => ({
       series: heatMapData,
+      options,
       // options: {
       //   ...preState.options,
       //   yaxis: {
@@ -202,10 +371,10 @@ class HeatmapChart extends React.Component {
     if (activeModal) {
       if (window.innerWidth > 1600) {
         height = 900;
-        width = 1500;
+        // width = 1500;
       } else if (window.innerWidth < 1600) {
         height = 570;
-        width = 1000;
+        // width = 1000;
         // } else if (window.innerWidth < 1600) {
         //   height = 570;
         //   width = 1000;
@@ -213,7 +382,17 @@ class HeatmapChart extends React.Component {
     } else {
       // height = 425;
       height = 550;
-      width = 450;
+      // width = 450;
+    }
+    if (!activeModal) {
+      if (showRightSidebar && window.innerWidth < 1600) width = 780;
+      else if (showRightSidebar && window.innerWidth > 1600)
+        width = 1100;
+      else if (!showRightSidebar && window.innerWidth < 1600)
+        width = 1100;
+      else width = 1400;
+    } else {
+      width = 1700;
     }
 
     return (
@@ -228,15 +407,15 @@ class HeatmapChart extends React.Component {
           type="heatmap"
           // height={!activeModal ? 550 : 650}
           height={height}
-          width={
-            showRightSidebar && window.innerWidth < 1600
-              ? 780
-              : showRightSidebar && window.innerWidth > 1600
-              ? 1100
-              : !showRightSidebar && window.innerWidth < 1600
-              ? 1100
-              : 1400
-          }
+          width={width}
+          // showRightSidebar && window.innerWidth < 1600
+          //   ? 780
+          //   : showRightSidebar && window.innerWidth > 1600
+          //   ? 1100
+          //   : !showRightSidebar && window.innerWidth < 1600
+          //   ? 1100
+          //   : 1400
+          // }
         />
       </div>
     );
