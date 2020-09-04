8ceec80c9cf274b57be6dc5e127febf85165c89f
palashdevmanandhar <palash.manandhar@gmail.com>
2020-09-04 11:51:14 +0545

    outreach admin division pop up has counts according to legend

--- src/js/components/Pages/OutreachExpansion/MapComponents/divisionInfoPopUp.js
+++ src/js/components/Pages/OutreachExpansion/MapComponents/divisionInfoPopUp.js
@@ -1,5 +1,8 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
+import Bother from '../../../../../img/Group5084.png';
+import Bblb from '../../../../../img/Group5086.png';
+import Bbank from '../../../../../img/Group5085.png';
 
 export default function divisionInfoPopUp(props) {
   const code = props.data;
@@ -17,22 +20,100 @@ export default function divisionInfoPopUp(props) {
             </div>
           )}
         </div>
-        {/* {props.mapViewDataBy === 'general_outreach' && (
-          <li style={{ marginBottom: '0 !important' }}>
-            <div className="organization-icon" />
-            <div className="organization-content">
-              <div className="org-header">
-                <>Commercial Bank</>
-                <div className="icon-list">
-                  <div className="icons">
-                    <i className="material-icons">tablet_mac</i>
-                    <>{props.data.bBank}</>
+        {props.mapViewDataBy === 'general_outreach' && (
+          <ul style={{ height: '100%', fontSize: '10px' }}>
+            {code.cBank > 0 && (
+              <li
+                style={{
+                  marginBottom: '0 !important',
+                  paddingRight: '0.5vw',
+                }}
+              >
+                <div className="organization-content">
+                  <div className="org-header">
+                    <>
+                      <img
+                        src={Bbank}
+                        alt=""
+                        height="17px"
+                        width="17px"
+                      />
+                      <span style={{ paddingLeft: '5px' }}>
+                        Commerical Bank Branch
+                      </span>
+                    </>
+                    <div className="icon-list">
+                      <div className="icons">
+                        <>{code.cBank}</>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </li>
-        )} */}
+              </li>
+            )}
+            {code.oBank > 0 && (
+              <li
+                style={{
+                  marginBottom: '0 !important',
+                  paddingRight: '0.5vw',
+                }}
+              >
+                <div className="organization-content">
+                  <div className="org-header">
+                    <>
+                      <img
+                        src={Bother}
+                        alt=""
+                        height="17px"
+                        width="17px"
+                      />
+                      <span style={{ paddingLeft: '5px' }}>
+                        {' '}
+                        Other Institution Branch
+                      </span>
+                    </>
+                    <div className="icon-list">
+                      <div className="icons">
+                        <>{code.oBank}</>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            )}
+
+            {code.cBlb > 0 && (
+              <li
+                style={{
+                  marginBottom: '0 !important',
+                  paddingRight: '0.5vw',
+                }}
+              >
+                <div className="organization-content">
+                  <div className="org-header">
+                    <>
+                      <img
+                        src={Bblb}
+                        alt=""
+                        height="17px"
+                        width="17px"
+                        // style={{ paddingRight: '5px' }}
+                      />
+                      <span style={{ paddingLeft: '5px' }}>
+                        Commerical Bank Blb
+                      </span>
+                    </>
+                    <div className="icon-list">
+                      <div className="icons">
+                        <>{code.cBlb}</>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            )}
+          </ul>
+        )}
       </div>
     </div>
   );
